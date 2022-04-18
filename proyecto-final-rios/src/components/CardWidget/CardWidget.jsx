8d21f7b76dbc7dee0bbb91e3d3393a4dd4fe97b8
Daniela Rios <daniela.rios.v91@gmail.com>
2022-04-18 12:06:03 -0400

    card widget and list item component

---
+++ proyecto-final-rios/src/components/CardWidget/CardWidget.jsx
@@ -0,0 +1,12 @@
+/** Clase que extiende de componente (rcc) */
+import React, { Component } from 'react'
+
+export default class CardWidget extends Component {
+    render() {
+        return (
+            <div>Esto es un CardWidget</div>
+        )
+    }
+}
+
+
---
+++ proyecto-final-rios/src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ItemListContainer extends Component {
+  render() {
+    return (
+      <div>ItemListContainer</div>
+    )
+  }
+}
